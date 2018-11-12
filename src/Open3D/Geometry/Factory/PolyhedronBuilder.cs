d8ea26eea18f5a29d21b6cbf19546b97b2baf919
Dzmitry Filipuk <dzmitry@filipuk.me>
2018-11-12 21:20:47 +0300

    figures builder made better

--- src/Open3D/Geometry/Factory/PolyhedronBuilder.cs
+++ src/Open3D/Geometry/Factory/PolyhedronBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Open3D.Geometry.Polyhedron;
 using Open3D.Math;
 
@@ -15,36 +16,12 @@ namespace Open3D.Geometry.Factory
         /// <param name="geometricCenter">Geometric center of figure.</param>
         /// <param name="rotationCenter">Rotation center of figure.</param>
         /// <returns></returns>
-        public static IPolyhedron3D CreateSimplePolyhedron(double a, double b, double c, HomogeneousPoint3D geometricCenter,
+        public static IPolyhedron3D CreateSimplePolyhedron(
+            double a, double b, double c, 
+            HomogeneousPoint3D geometricCenter,
             HomogeneousPoint3D rotationCenter)
         {
-            var polyhedron = new SimplePolyhedron3D(
-                rotationCenter,
-                new[]
-                {
-                    new HomogeneousPoint3D(0, 0, 0, 1),
-                    new HomogeneousPoint3D(0, 0, c, 1),
-                    new HomogeneousPoint3D(a, 0, c, 1),
-                    new HomogeneousPoint3D(a, 0, 0, 1),
-                    new HomogeneousPoint3D(0, b, 0, 1),
-                    new HomogeneousPoint3D(0, b, c, 1),
-                    new HomogeneousPoint3D(a, b, c, 1),
-                    new HomogeneousPoint3D(a, b, 0, 1),
-                },
-                new[]
-                {
-                    new [] { 0, 3, 2, 1 },
-                    new [] { 4, 5, 6, 7 },
-                    new [] { 0, 1, 5, 4 },
-                    new [] { 7, 6, 2, 3 },
-                    new [] { 0, 4, 7, 3 },
-                    new [] { 1, 2, 6, 5 },
-                },
-                (0, 6));
-
-            MovePolyhedronGeometricCenter(new HomogeneousPoint3D(a / 2, b / 2, c / 2, 1), geometricCenter, polyhedron);
-
-            return polyhedron;
+            return CreateParallelepiped(a, b, c, geometricCenter, rotationCenter, new int[] {2,3});
         }
 
         /// <summary>
@@ -56,58 +33,128 @@ namespace Open3D.Geometry.Factory
         /// <param name="geometricCenter">Geometric center of figure.</param>
         /// <param name="rotationCenter">Rotation center of figure.</param>
         /// <returns></returns>
-        public static IPolyhedron3D CreateCompositePolyhedron(double a, double b, double c,
+        public static IPolyhedron3D CreateCompositePolyhedron(
+            double a, double b, double c,
             HomogeneousPoint3D geometricCenter,
             HomogeneousPoint3D rotationCenter)
         {
-            var delta = 0;
+            return CreateParallelepipedWithHole(a, b, c, geometricCenter, rotationCenter, 0, false);
+        }
 
-            var polyhedrons = new List<IPolyhedron3D>
+        private static IPolyhedron3D CreateParallelepipedWithHole(
+            double a, double b, double c,
+            HomogeneousPoint3D geometricCenter,
+            HomogeneousPoint3D rotationCenter,
+            double distance, bool showInvisibleFacets)
+        {
+            var parallelepiped = new List<IPolyhedron3D>
             {
-                //CreateSimplePolyhedron(a, b, c / 3,
-                //    new HomogeneousPoint3D(0, 0, -2 * c / 6, 1),
-                //    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                //CreateSimplePolyhedron(a, b, c / 3,
-                //    new HomogeneousPoint3D(0, 0, 2 * c / 6, 1),
-                //    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                //CreateSimplePolyhedron(a / 3, b, c / 3,
-                //    new HomogeneousPoint3D(-2 * a / 6, 0, 0, 1),
-                //    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                //CreateSimplePolyhedron(a / 3, b, c / 3,
-                //    new HomogeneousPoint3D(2 * a / 6, 0, 0, 1),
-                //    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W))
-
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(0, 0, -2 * c / 6 + delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(0, 0, 2 * c / 6 - delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(-2 * a / 6 + delta, 0, 0, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(2 * a / 6 - delta, 0, 0, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(-2 * a / 6 + delta, 0, -2 * c / 6 + delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(2 * a / 6 - delta, 0, -2 * c / 6 + delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(2 * a / 6 - delta, 0, 2 * c / 6 - delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
-                CreateSimplePolyhedron(a / 3, b, c / 3,
-                    new HomogeneousPoint3D(-2 * a / 6 + delta, 0, 2 * c / 6 - delta, 1),
-                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W)),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(0, 0, -2 * c / 6 - distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {2,3}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(0, 0, 2 * c / 6 + distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {2,3}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(-2 * a / 6 - distance, 0, 0, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {0,1}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(2 * a / 6 + distance, 0, 0, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {0,1}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(-2 * a / 6 - distance, 0, -2 * c / 6 - distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {1,2}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(2 * a / 6 + distance, 0, -2 * c / 6 - distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {1,3}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(2 * a / 6 + distance, 0, 2 * c / 6 + distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {0,3}),
+                CreateParallelepiped(
+                    a / 3, b, c / 3,
+                    new HomogeneousPoint3D(-2 * a / 6 - distance, 0, 2 * c / 6 + distance, 1),
+                    new HomogeneousPoint3D(rotationCenter.X, rotationCenter.Y, rotationCenter.Z, rotationCenter.W),
+                    showInvisibleFacets ? new int[0] : new [] {0,2}),
             };
 
-            var result = new CompositePolyhedron3D(rotationCenter, polyhedrons, (0, 1));
+            var result = new CompositePolyhedron3D(rotationCenter, parallelepiped, (0, 1));
 
             MovePolyhedronGeometricCenter(new HomogeneousPoint3D(0, 0, 0, 1), geometricCenter, result);
 
             return result;
+        }
+
+        /// <summary>
+        /// 0 - Front
+        /// 1 - Back
+        /// 2 - Right
+        /// 3 - Left
+        /// 4 - Top
+        /// 5 - Bottom
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="geometricCenter"></param>
+        /// <param name="rotationCenter"></param>
+        /// <param name="notVisibleFacets"></param>
+        /// <returns></returns>
+        private static IPolyhedron3D CreateParallelepiped(
+            double a, double b, double c,
+            HomogeneousPoint3D geometricCenter,
+            HomogeneousPoint3D rotationCenter, 
+            int[] notVisibleFacets)
+        {
+            var allFacets = new List<int[]>
+            {
+                new[] {0, 4, 7, 3},
+                new[] {1, 2, 6, 5},
+                new[] {7, 6, 2, 3},
+                new[] {0, 1, 5, 4},
+                new[] {0, 3, 2, 1},
+                new[] {4, 5, 6, 7},    
+            };
+            var visibleFacets = new List<int[]>();
+
+            for (int i = 0; i < allFacets.Count; i++)
+            {
+                if (!notVisibleFacets.Contains(i))
+                {
+                    visibleFacets.Add(allFacets[i]);
+                }
+            }
+
+            var parallelepiped = new SimplePolyhedron3D(
+                rotationCenter,
+                new[]
+                {
+                    new HomogeneousPoint3D(0, 0, 0, 1),
+                    new HomogeneousPoint3D(0, 0, c, 1),
+                    new HomogeneousPoint3D(a, 0, c, 1),
+                    new HomogeneousPoint3D(a, 0, 0, 1),
+                    new HomogeneousPoint3D(0, b, 0, 1),
+                    new HomogeneousPoint3D(0, b, c, 1),
+                    new HomogeneousPoint3D(a, b, c, 1),
+                    new HomogeneousPoint3D(a, b, 0, 1),
+                }, visibleFacets, (0, 6));
+
+            MovePolyhedronGeometricCenter(new HomogeneousPoint3D(a / 2, b / 2, c / 2, 1), geometricCenter, parallelepiped);
+
+            return parallelepiped;
         }
 
         private static void MovePolyhedronGeometricCenter(HomogeneousPoint3D currentCenter,
